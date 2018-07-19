c05b0003f311ec06f181fdf75411b9733ca77a6e
weiki <wen18826478504@163.com>
2018-07-19 23:43:51 +0800

    首页改版

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,9 @@ var App = getApp();
 Page({
 
     data: {
-        imgUrls: [
-            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-        ],
+		imgUrls:['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+				'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+				'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'],
 		cases:[
 			"全部案例","开发类","设计类","市场&运营类","产品类"
 		],
@@ -16,46 +14,85 @@ Page({
         interval: 5000,
         duration: 1000,
 		x:0,
-		activity:0
+		activity:0,
+		width: 100,
+		caseSlideHeight:'400px',
+		currentCase:0,
     },
+
+	casesSlideChange:function(event){
+		const idx = event.detail.current;
+		const that = this;
+		const width = this.data.width;
+		if (idx < this.activity){
+			this.setData({
+				activity: idx,
+				x : (width * idx)
+			});
+		} else {
+			this.setData({
+				activity: idx,
+				x: -(width * idx)
+			});
+		}
+	},
+	imageLoad:function(e){
+		const imageWidth = e.detail.width;
+		const imageHeight = e.detail.height;
+		const factImageWidth = wx.getSystemInfoSync().windowWidth;
+		const windownHeight = wx.getSystemInfoSync().windowHeight;
+		const factImageHeight = factImageWidth / imageWidth * imageHeight;
+		this.setData({
+			factImageHeight : factImageHeight,
+			caseSlideHeight: windownHeight - factImageHeight
+		});
+	},
 
 	switchCase: function (event)
 	{
-		const width = 100;
+		const width = this.data.width;
 		const distanceX = event.detail.x;
 		const index = event.currentTarget.dataset.index;
 		if (distanceX < width) {
 			this.setData({
 				activity:index,
-				x: width
+				x: width,
+				currentCase:index
 			});
 		} else {
 			this.setData({
 				activity: index,
-				x: - width * index
+				x: - width * index,
+				currentCase: index
 			});
 		}
-		
+	},
+
+	caseDetail:function(){
+		wx.showModal({
+			title: '油麦菜',
+			content: '精彩内容，敬请期待',
+		})
 	},
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+		const that = this;
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function() {
-
+		
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function() {
-
+		console.log(this.data.factImageHeight);
     },
 
     /**
