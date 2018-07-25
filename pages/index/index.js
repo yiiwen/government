dcf672c75f8efc8e7dae4beb9ca936ecdacd1b77
yiiwen <18825179513@163.com>
2018-07-25 19:29:47 +0800

    新闻模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,9 @@ var App = getApp();
 Page({
 
     data: {
-		imgUrls:['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-				'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-				'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'],
+      imgUrls: ['/images/index.jpg',
+        '/images/index.jpg',
+        '/images/index.jpg'],
 		cases:[
 			"全部案例","开发类","设计类","市场&运营类","产品类"
 		],
